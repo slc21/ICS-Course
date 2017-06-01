942e2f454474b3b5c02087507f0f2a8301406ebc
Saul <slc21@berkeley.edu>
2017-05-31 21:08:54 -0700

    Ch5 assignment

---
+++ ch5/biggerBetterFavoriteNumber.rb
@@ -0,0 +1,4 @@
+puts 'What\'s your favorite number?'
+num1 = gets.chomp.to_i
+add = num1 + 1
+puts 'This is a bigger and better number ' + add.to_s
---
+++ ch5/fullNameGreeting.rb
@@ -0,0 +1,7 @@
+puts 'What\'s your name?'
+name = gets.chomp
+puts 'Hello ' + name + ', What\'s your middle name?'
+name2 = gets.chomp
+puts 'Thanks' + ' ' + name + ' ' + name2 + ', What about last name?'
+name3 = gets.chomp
+puts 'Thanks' + ' ' + name + ' ' + name2 + ' ' + name3 + ', pleased to meet you'
