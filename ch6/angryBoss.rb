8a12045eafec5496b1060d0da4622399997e6f2f
Saul <slc21@berkeley.edu>
2017-06-06 19:10:20 -0700

    finished 1st hw

---
+++ ch6/angryBoss.rb
@@ -0,0 +1,3 @@
+puts "WHADDYA WANT?!?!"
+req =gets.chomp
+puts "WHADDYA MEAN " + req.upcase + "!!! " + "YOU\'RE FIRED!!!"
---
+++ ch6/homwrk.rb
@@ -0,0 +1,4 @@
+puts "WHADDYA WANT?!?!"
+frdm ="no homework"
+puts frdm
+puts "WHADDYA MEAN " + frdm.upcase + "!?!?! " + "YOU\'RE GOING TO BERKELEY!!!"
