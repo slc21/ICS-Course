a39ad4d2bc202e482a635470b80c4ad078fd3387
Saul <slc21@berkeley.edu>
2017-06-30 12:55:14 -0700

    hw part 2

---
+++ ch12/birthdayHelper.rb
@@ -0,0 +1,10 @@
+filename = 'Birthdays'
+
+dates = ['Christopher Alexander,  Oct4,1936
+          ChristopherLambert,     Mar29,1957
+          ChristopherLee,         May27,1922
+          ChristopherLloyd,       Oct22,1938
+          ChristopherPine,        Aug3,1976
+          ChristopherPlummer,     Dec13,1927
+          ChristopherWalken,      Mar31,1943
+          TheKingofSpain,         Jan5,1938']
